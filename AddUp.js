ba427f8b7b7bfabde04a76cd2e908085e4251f93
mohabashraf <mohabashraf466@gmail.com>
2020-07-09 08:10:18 +0200

    add test for add up with jest

--- AddUp.js
+++ AddUp.js
@@ -1,4 +1,5 @@
-//  const {performance} = require('pref_hooks');
+const { TestScheduler } = require("jest");
+
 function addUp(number) {
 	return number * (number + 1) / 2;
 }
@@ -6,3 +7,6 @@ let t1 = Date.now();
 console.log(addUp(6));
 let t2 = Date.now();
 console.log(`Time Elapsed: ${ (t2 - t1) / 1000} seconds`);
+
+module.exports = addUp
+
---
+++ AddUp.test.js
@@ -0,0 +1,8 @@
+const AddUp = require('./AddUp');
+const addUp = require('./AddUp');
+
+test(
+	'The Add UP for 6 is 21', () => {
+		expect(addUp(6)).toBe(21);
+	}
+);
