8a9ab16c2065a703ce444ce63c0ea12ada4461c9
mohabashraf <mohabashraf466@gmail.com>
2020-07-18 15:39:08 +0200

    BMI challenge

---
+++ CodeChalenge.js
@@ -0,0 +1,5 @@
+var markBMI, johnBMI, markMass, johnMass, markHeight, johnHeight = 0; 0; 60; 60; 180; 180;
+markBMI = markMass / (markHeight * markHeight);
+johnBMI = johnMass / (johnHeight * johnHeight);
+const boolMarkhigherBMIThanJohn = markBMI > johnBMI;
+console.log("is Mark BMI Greater than John?" + boolMarkhigherBMIThanJohn);
